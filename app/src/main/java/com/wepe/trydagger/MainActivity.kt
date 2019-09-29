8ff344f48e9a83ca545ad63bd4dc16cf331ffa1f
Wahyu Permadi <wahyu@medigo.id>
2019-09-29 23:13:08 +0700

    first commit

---
+++ app/src/main/java/com/wepe/trydagger/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wepe.trydagger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
