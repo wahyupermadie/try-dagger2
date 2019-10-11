3c1c06679b3b4cf8b7b22f5ebcbd978cd68cde45
Wahyu Permadi <wahyu@medigo.id>
2019-10-11 16:55:15 +0800

    [ADD] resource handler

---
+++ app/src/main/java/com/wepe/trydagger/utils/Resource.kt
@@ -0,0 +1,35 @@
+package com.wepe.trydagger.utils
+
+data class Resource<out T>(val status: Status, val data: T?, val error: String?) {
+    companion object {
+        fun <T> success(data: T?): Resource<T> {
+            return Resource(
+                Status.SUCCESS,
+                data,
+                null
+            )
+        }
+
+        fun <T> error(error: String, data: T?): Resource<T> {
+            return Resource(
+                Status.ERROR,
+                data,
+                error
+            )
+        }
+
+        fun <T> loading(data: T?): Resource<T> {
+            return Resource(
+                Status.LOADING,
+                data,
+                null
+            )
+        }
+    }
+
+    enum class Status {
+        SUCCESS,
+        ERROR,
+        LOADING
+    }
+}
