5c6e39a1d3d12548783b479efe122764bda7f64c
Wahyu Permadi <wahyu@medigo.id>
2019-10-12 15:03:43 +0800

    [ADD] movies vm

--- app/src/main/java/com/wepe/trydagger/ui/movies/list/MoviesFragment.kt
+++ app/src/main/java/com/wepe/trydagger/ui/movies/list/MoviesFragment.kt
@@ -1,9 +1,11 @@
 package com.wepe.trydagger.ui.movies.list
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.wepe.trydagger.base.BaseFragment
 import com.wepe.trydagger.base.BaseViewModel
@@ -26,5 +28,13 @@ class MoviesFragment : BaseFragment(){
         binding = FragmentMoviesBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.getMovies(1)
+        viewModel.movies.observe(viewLifecycleOwner, Observer {
+            Log.d("DATA_GUE","DATA "+it)
+        })
     }
 }
--- app/src/main/java/com/wepe/trydagger/ui/movies/viewmodel/MoviesViewModel.kt
+++ app/src/main/java/com/wepe/trydagger/ui/movies/viewmodel/MoviesViewModel.kt
@@ -1,8 +1,36 @@
 package com.wepe.trydagger.ui.movies.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.wepe.trydagger.BuildConfig
 import com.wepe.trydagger.base.BaseViewModel
+import com.wepe.trydagger.data.model.ResponseMovies
+import com.wepe.trydagger.domain.MoviesDomain
+import com.wepe.trydagger.utils.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class MoviesViewModel @Inject constructor() : BaseViewModel(){
+class MoviesViewModel @Inject constructor(
+    private val moviesDomain: MoviesDomain
+) : BaseViewModel(){
 
+    private val _movies = MediatorLiveData<Resource<ResponseMovies>>()
+    val movies : LiveData<Resource<ResponseMovies>> = _movies
+
+    private var moviesSource: LiveData<Resource<ResponseMovies>> = MutableLiveData<Resource<ResponseMovies>>()
+
+    fun getMovies(page : Int) = viewModelScope.launch(Dispatchers.Main){
+        _movies.postValue(null)
+        _movies.removeSource(moviesSource)
+        withContext(Dispatchers.IO){
+            moviesSource = moviesDomain.fetchMovies(page, BuildConfig.API_KEY)
+        }
+        _movies.addSource(moviesSource){
+            _movies.value = it
+        }
+    }
 }
