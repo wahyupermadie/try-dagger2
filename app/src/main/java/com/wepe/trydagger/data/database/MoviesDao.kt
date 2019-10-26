500e5a9243302b4e7faf9e51b895f9052c8eda2d
= <wahyu.permadi1725@gmail.com>
2019-10-26 19:48:46 +0700

    [ADD} paging

--- app/src/main/java/com/wepe/trydagger/data/database/MoviesDao.kt
+++ app/src/main/java/com/wepe/trydagger/data/database/MoviesDao.kt
@@ -1,5 +1,6 @@
 package com.wepe.trydagger.data.database
 
+import androidx.paging.DataSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.REPLACE
@@ -10,7 +11,7 @@ import com.wepe.trydagger.data.model.ResultsMovies
 interface MoviesDao {
 
     @Query("SELECT * FROM movies")
-    suspend fun getMovies() : List<ResultsMovies>
+    suspend fun getMovies() : DataSource.Factory<Int, ResultsMovies>
 
     @Insert(onConflict = REPLACE)
     suspend fun insert(movies: ResultsMovies) : Long
--- app/src/main/java/com/wepe/trydagger/data/remote/MoviesRepository.kt
+++ app/src/main/java/com/wepe/trydagger/data/remote/MoviesRepository.kt
@@ -2,6 +2,7 @@ package com.wepe.trydagger.data.remote
 
 import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.paging.PagedList
 import com.google.gson.Gson
 import com.wepe.trydagger.data.database.MoviesDao
 import com.wepe.trydagger.data.model.ResponseMovies
@@ -13,7 +14,7 @@ import retrofit2.Response
 import javax.inject.Inject
 
 interface MoviesRepository{
-    suspend fun getMovies(page: Int, apiKey: String) : LiveData<Resource<List<ResultsMovies>>>
+    suspend fun getMovies(page: Int, apiKey: String) : LiveData<Resource<PagedList<ResultsMovies>>>
 }
 class MoviesRepositoryImpl @Inject constructor(
     private val apiService: ApiService,
@@ -21,27 +22,24 @@ class MoviesRepositoryImpl @Inject constructor(
     private val moviesDao: MoviesDao,
     private val context: Context
 ): MoviesRepository {
-    override suspend fun getMovies(page: Int, apiKey: String) : LiveData<Resource<List<ResultsMovies>>>{
-        return object : NetworkBoundResource<List<ResultsMovies>, ResponseMovies>(gson) {
+    override suspend fun getMovies(page: Int, apiKey: String) : LiveData<Resource<PagedList<ResultsMovies>>>{
+        return object : NetworkBoundResource<PagedList<ResultsMovies>, ResponseMovies>(gson) {
             override suspend fun saveCallResult(item: ResponseMovies) {
-                item.let {
-                    it.results?.forEach {movie ->
-                        moviesDao.insert(movie)
-                    }
-                }
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
-            override fun shouldFetch(data: List<ResultsMovies>?): Boolean {
-                return Constants.isConnected(context)
+            override fun shouldFetch(data: PagedList<ResultsMovies>?): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
-            override suspend fun loadFromDb(): List<ResultsMovies> {
-                return moviesDao.getMovies()
+            override suspend fun loadFromDb(): PagedList<ResultsMovies> {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override suspend fun createCallAsync(): Response<ResponseMovies> {
-                return apiService.getPopularMovies(apiKey, page)
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
+
         }.build().asLiveData()
     }
 }
